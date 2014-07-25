43ed6b1bc1d1f47c21adde474e41becdb06b5fc2
Jere Menichelli <jmenichelli@gmail.com>
2014-07-25 00:18:14 -0300

    Closure added. Improved checkpoints

--- src/chronos.js
+++ src/chronos.js
@@ -1,5 +1,6 @@
-var Chronos = function(){
-    this.units = {
+(function(root){
+    var Chronos = function(){
+        this.units = {
             hours: {
                 factor: 3600000000,
                 label: 'h'
@@ -15,53 +16,59 @@ var Chronos = function(){
                 include: true
             }
         };
-};
-
-Chronos.prototype.init = function(options){
-    this.initDate = new Date(),
-    this.label = options.label || 'Chronos new instance';
-    this.units.hours.include = options.showHours || false;
-
-    console.log(this.label + ' initialized');
-};
-
-Chronos.prototype.calculateCheckpoint = function(diff){
-    var t = {};
-
-    for(var key in this.units){
-        if(this.units[key].include){
-            var coefficient = Math.floor(diff/this.units[key].factor)
-            t[key] = coefficient;
-            diff -= coefficient*this.units[key].factor;
-        }
     };
 
-    t.milliseconds = diff;
+    Chronos.prototype.init = function(opt){
+        var options = opt || {};
+        this.initDate = new Date(),
+        this.label = options.label || 'Chronos new instance';
+        this.units.hours.include = options.showHours || false;
 
-    return t;
-}
-
-Chronos.prototype.checkpoint = function(){
-    var now = new Date(),
-        diff = now.getTime() - this.initDate.getTime(),
-        msg = this.label + ' checkpoint: +',
-        t;
-
-    t = this.calculateCheckpoint(diff);
-
-    for(var key in this.units){
-        if(this.units[key].include){
-            msg += t[key] + this.units[key].label + ' ';
-        }
+        console.log(this.label + ' initialized');
     };
 
-    msg += t.milliseconds + 'ms';
+    Chronos.prototype._calculateCheckpoint = function(diff){
+        var t = {};
 
-    console.log(msg);
-}
+        for(var key in this.units){
+            if(this.units[key].include){
+                var coefficient = Math.floor(diff/this.units[key].factor)
+                t[key] = coefficient;
+                diff -= coefficient*this.units[key].factor;
+            }
+        };
+
+        t.milliseconds = diff;
+
+        return t;
+    }
+
+    Chronos.prototype.checkpoint = function(checkpointLabel){
+        var now = new Date(),
+            diff = now.getTime() - this.initDate.getTime(),
+            checkpointLabel = checkpointLabel || 'checkpoint',
+            msg = this.label + ' ' + checkpointLabel + ': ',
+            t;
+
+        t = this._calculateCheckpoint(diff);
+
+        for(var key in this.units){
+            if(this.units[key].include){
+                msg += t[key] + this.units[key].label + ' ';
+            }
+        };
+
+        msg += t.milliseconds + 'ms';
+
+        console.log(msg);
+    };
+    
+    root.Chronos = Chronos;
+
+})(this, undefined);
 
 
 // test
 var timer = new Chronos();
-timer.init({ label: 'timer' });
-timer.checkpoint();
+timer.init({ label: 'timer', showHours: true });
+timer.checkpoint('quick check');
